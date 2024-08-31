d4065011e213859f9a3a5d777d285209a7b509ef
mysf10 <myh481813@gmail.com>
2024-08-31 08:32:17 +0700

    first commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract SimpleStorage {
+    uint256 public number;
+
+    mapping(string => uint256) public stringToNumber;
+
+    struct People {
+        string name;
+        uint256 number;
+    }
+
+    People[] public people;
+
+    function addPeople(string memory _name, uint256 _number) public {
+        people.push(People(_name, _number));
+        stringToNumber[_name] = _number;
+    }
+
+    function store(uint256 _number) public {
+        number = _number;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
