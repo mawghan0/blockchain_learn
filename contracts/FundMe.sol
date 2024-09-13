2dd98bf40070d745354a7d1c16d676b9c59ff616
mysf10 <myh481813@gmail.com>
2024-09-13 09:59:41 +0700

    add withdrawFunction

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -13,6 +13,29 @@ contract FundMe {
     function fund() public payable {
         require(msg.value.getConvertionRate() > usd, "error, must be 1eth");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        // mapping reset
+        for (uint256 i = 0; i < funders.length; i++) {
+            address funder = funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        // funders reset
+        funders = new address[](0);
+
+        // withdraw
+        // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // send
+        bool send = payable(msg.sender).send(address(this).balance);
+        require(send, "send reverted");
+
+        // call
+        (bool call, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(call, "send reverted");
     }
 }
